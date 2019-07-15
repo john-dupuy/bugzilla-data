7df35c2c33420a373c071a60772a26f57c026262
john-dupuy <jdupuy@redhat.com>
2019-07-15 11:17:33 -0400

    Add ability to use saved search and login to Bugzilla

--- bz_data.py
+++ bz_data.py
@@ -5,7 +5,9 @@ import sys
 import textwrap
 import yaml
 
+from bugzilla.transport import BugzillaError
 from cached_property import cached_property
+from contextlib import contextmanager
 from itertools import chain
 from matplotlib.offsetbox import AnchoredText
 
@@ -13,11 +15,15 @@ from matplotlib.offsetbox import AnchoredText
 class BugzillaData:
     """ Class representation of BZ Data, contains methods for plotting."""
 
-    def __init__(self, query_file, url):
+    def __init__(self, query_file, url, plot_style, **kwargs):
         self.query_file = query_file
         self.bzapi = bugzilla.Bugzilla(url)
+        self.plot_style = plot_style
+        self.login_required = kwargs.get("login")
+        self.credential_file = kwargs.get("credential_file")
         self.query = None
         self.queries = None
+        # parse the query file
         try:
             with open(self.query_file, "r") as stream:
                 queries = yaml.load(stream, Loader=yaml.FullLoader)
@@ -30,27 +36,59 @@ class BugzillaData:
         except IOError:
             print("IOError: Query file not present at {}, exiting...".format(self.query_file))
             sys.exit(1)
+        # parse login info, if there is any
+        try:
+            with open(self.credential_file, "r") as stream:
+                creds = yaml.load(stream, Loader=yaml.FullLoader)[0]
+            self.creds = creds.get("login_info")
+        except IOError:
+            self.creds = None
+
+    @contextmanager
+    def logged_into_bugzilla(self):
+        # login to bzapi
+        if not self.creds:
+            # error out if there are no creds available in yamls
+            raise BugzillaError("No creds available to log into Bugzilla")
+        try:
+            yield self.bzapi.login(self.creds.get("username"), self.creds.get("password"))
+        except BugzillaError:
+            raise
+        else:
+            self.bzapi.logout()
 
     @cached_property
     def bugs(self):
         bugs = []
-        if self.queries:
-            for query in self.queries:
-                print(query)
-                bugs.extend(self.bzapi.query(query))
+        if self.login_required:
+            with self.logged_into_bugzilla():
+                if self.queries:
+                    for query in self.queries:
+                        print(query)
+                        bugs.extend(self.bzapi.query(query))
+                else:
+                    bugs.extend(self.bzapi.query(self.query))
         else:
-            bugs.extend(self.bzapi.query(self.query))
+            if self.queries:
+                for query in self.queries:
+                    print(query)
+                    bugs.extend(self.bzapi.query(query))
+            else:
+                bugs.extend(self.bzapi.query(self.query))
         return bugs
 
     @property
     def title(self):
-        title = "{status} BZ's"
+        title = "{status} BZ's sorted by {plot_style}"
         if self.queries:
             status = [query.get("status", "") for query in self.queries]
             status = list(dict.fromkeys(list(chain.from_iterable(status))))
         else:
             status = self.query.get("status", "")
-        return title.format(status=", ".join(map(str, status)))
+        return title.format(
+            status=", ".join(map(str, status)),
+            plot_style=self.plot_style.capitalize()
+        )
 
     @property
     def product(self):
@@ -61,8 +99,8 @@ class BugzillaData:
             product = self.query.get("product", "")
         return ", ".join(map(str, product))
 
-    def get_plot_data(self, plot_style):
-        bz_data = [getattr(bug, plot_style) for bug in self.bugs]
+    def get_plot_data(self):
+        bz_data = [getattr(bug, self.plot_style) for bug in self.bugs]
         bz_counts = {
             attr: bz_data.count(attr) for attr in bz_data
         }
@@ -70,8 +108,8 @@ class BugzillaData:
         xvals = range(len(sorted_counts))
         return xvals, sorted_counts
 
-    def generate_plot(self, plot_style, save=False):
-        xvals, sorted_counts = self.get_plot_data(plot_style)
+    def generate_plot(self, save=False):
+        xvals, sorted_counts = self.get_plot_data()
         # create the figure
         fig, ax = plt.subplots()
         ax.bar(xvals, [s[1] for s in sorted_counts], align="center")
@@ -82,7 +120,7 @@ class BugzillaData:
             ax.add_artist(AnchoredText(self.product, loc=1))
         plt.tight_layout()
         if save:
-            plt.savefig("{}.png".format(plot_style))
+            plt.savefig("{}.png".format(self.plot_style))
         plt.show()
 
 
@@ -111,9 +149,24 @@ def main():
         "--output", action="store_true", default=False,
         help="Output bugzilla data from query to stdout"
     )
+    parser.add_argument(
+        "--login", action="store_true", default=False,
+        help="Login to Bugzilla before making query. Required to use e.g. savedsearch and to get"
+             "some hidden fields."
+    )
+    parser.add_argument(
+        "--credential_file", type=str, default="conf/credentials.yaml",
+        help="Path to credential yaml file"
+    )
     args = parser.parse_args()
     # instantiate object
-    bz_data = BugzillaData(args.query, args.url)
+    bz_data = BugzillaData(
+        args.query,
+        args.url,
+        args.plot,
+        login=args.login,
+        credential_file=args.credential_file
+    )
     # print out info if necessary
     if args.output:
         for bug in bz_data.bugs:
@@ -134,7 +187,7 @@ def main():
             )
             print(textwrap.dedent(bug_string))
     # generate the plot
-    bz_data.generate_plot(args.plot, save=args.save)
+    bz_data.generate_plot(save=args.save)
 
 
 if __name__ == "__main__":
